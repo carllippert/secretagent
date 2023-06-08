fcd18065c211dd1ad4736a8a07cfe33828491524
Carl Lippert <carl.lippert@gmail.com>
2023-06-08 17:30:11 -0300

    Successfully reading from file system on mac

--- src/Views/MarkdownEditor.jsx
+++ src/Views/MarkdownEditor.jsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MDEditor from "@uiw/react-md-editor";
+import { useTauriContext } from "../TauriProvider";
+import { readDir, BaseDirectory } from "@tauri-apps/api/fs";
+import { join } from "@tauri-apps/api/path";
+import {
+  APP_NAME,
+  notify,
+  RUNNING_IN_TAURI,
+  useMinWidth,
+  useStorage,
+} from "../utils";
+
+// Reads the `$APPDATA/users` directory recursively
 
 export default function App() {
+  const { fileSep, loading, documents, downloads, appDocuments } =
+    useTauriContext();
+
+  const [markdownFiles, setMarkdownFiles] = useState([]);
+
+  // store-plugin will create necessary directories
+  // const storeName = `${documents}${APP_NAME}${fileSep}example_view.dat`;
+  // // const storeName = 'data.dat';
+  // const [data, setData, loadingData] = useStorage("exampleKey", "", storeName);
+
   const [value, setValue] = React.useState("**Hello world!!!**");
+
+  const getDir = async () => {
+    try {
+      const entries = await readDir(appDocuments + "/markdown", {
+        recursive: true,
+      });
+
+      for (const entry of entries) {
+        console.log("Entry", entry);
+
+        console.log(`Entry: ${entry.path}`);
+        if (entry.children) {
+          processEntries(entry.children);
+        }
+      }
+      setMarkdownFiles(entries);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getDir();
+  }, []);
+
   return (
     <div className="container">
       <MDEditor value={value} onChange={setValue} />
